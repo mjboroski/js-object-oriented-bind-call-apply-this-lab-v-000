44c9a1736fae963f322588f33b42bb0f437da050
Michael Boroski <mjboroski@gmail.com>
2018-07-09 19:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+function justInvoke(fn){
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg){
+  return justInvoke.call(fn, thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, arg){
+  return justInvoke.apply(fn, [thisValue, arg]);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  
+}
