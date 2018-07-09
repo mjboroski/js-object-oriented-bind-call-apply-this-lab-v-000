b233b97f979c63132a88565a440f36756d4500b2
Michael Boroski <mjboroski@gmail.com>
2018-07-09 19:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg);
 }
 
-function setThisWithApply(fn, thisValue, argument){
-  return fn.apply(thisValue, [argument]);
+function setThisWithApply(fn, thisValue, arg){
+  return fn.apply(thisValue, arg);
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue){
